046a05677688fc9d00a382d4cf9414286e013c73
Greg Nishihara <gnishihara@gmail.com>
2021-04-17 11:42:56 +0900

    Another indexing fix.

--- create_histogram_mp4.sh
+++ create_histogram_mp4.sh
@@ -2,7 +2,7 @@
 
 FOLDER="/home/Lab_Data/videofile/fish_videos/histogram"
 
-ffmpeg -r ${2} -i pngout/pngout_%05d.png \
+ffmpeg -start_number 0 -r ${2} -i pngout/pngout_%05d.png \
   -c:v libx264 -an \
   -vf "crop='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p" \
   ${FOLDER}/${1}_histogram.mp4 -y && \
